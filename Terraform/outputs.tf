a909538b9830b77a25a08e12ecd7ffbc1ee31fb6
Lee-Mang <leenobec20@gmail.com>
2025-03-01 21:20:11 +0100

    edit

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,7 +3,3 @@ output "server_ipv4" {
   description = "IPv4 address of the server droplet"
 }
 
-output "server_id" {
-  value       = digitalocean_droplet.server.id
-  description = "ID of the server droplet"
-}
