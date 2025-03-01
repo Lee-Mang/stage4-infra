d7a7a084e7d05bcfc9c64ee62235f2deaa70a1b4
Lee-Mang <leenobec20@gmail.com>
2025-03-01 16:17:10 +0100

    updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,58 @@
+# main.tf
+
+provider "digitalocean" {
+  token = "dop_v1_90b25d425f4e1e6059c670bee45e830521e8fc80cfb4f9853685dac013a68738"
+}
+
+resource "digitalocean_droplet" "example" {
+  image  = "ubuntu-24-10-x64"
+  name   = "ubuntu-HNG"
+  region = "lon1"  # Use your preferred region
+  size   = "s-1vcpu-2gb"
+  ssh_keys = ["HNG"]  # Replace with your SSH key ID
+}
+
+output "droplet_ip" {
+  value = digitalocean_droplet.example.ipv4_address
+}
+
+resource "digitalocean_firewall" "example" {
+  name    = "example-firewall"
+  droplet_ids = [digitalocean_droplet.my_droplet.id]  # This attaches the firewall to your droplet
+
+  inbound_rule {
+    protocol   = "tcp"
+    port_range = "22"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ inbound_rule {
+    protocol    = "tcp"
+    port_range  = "80"
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol    = "tcp"
+    port_range  = "443"
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
 
